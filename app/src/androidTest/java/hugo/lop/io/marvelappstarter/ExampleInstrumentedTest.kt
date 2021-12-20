047ac014fd2c6f8de67122df593d631eb238529e
Hugo Costa <costarafaelhugo@gmail.com>
2021-12-20 15:48:45 -0300

    Mudei os nomes das pastas

--- app/src/androidTest/java/hugo/lop/io/marvelappstarter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/hugo/lop/io/marvelappstarter/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package daniel.lop.io.marvelappstarter
+package hugo.lop.io.marvelappstarter
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
