9bf8fd1170af6fda9e585456a16be7226bfb3120
Hugo Costa <costarafaelhugo@gmail.com>
2021-12-28 10:52:30 -0300

    Criei o arquivo de extensões

---
+++ app/src/main/java/hugo/lop/io/marvelappstarter/util/extension.kt
@@ -0,0 +1,22 @@
+package hugo.lop.io.marvelappstarter.util
+
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import java.time.Duration
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_LONG){
+    Toast.makeText(
+        requireContext(),
+        message,
+        duration
+    ).show()
+}
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.hide(){
+    visibility = View.INVISIBLE
+}
